31cbe7cfe8eea693b47d4e5eae1542c179b248da
Unknown <g1827402@st.t-kougei.ac.jp>
2018-05-02 18:02:30 +0900

    文字列、if、for文のテスト

---
+++ web1-3/Assets/test1_3.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class test1_3 : MonoBehaviour
+{
+
+    // Use this for initialization
+    void Start()
+    {
+        Debug.Log("hellow,world!");
+
+        int age = 30, hp = 100, damage = 10, i;
+
+        float height1 = 160.5f, height2;
+
+        string name = "takabe";
+
+        height2 = height1;
+
+        Debug.Log("名前："+name);
+        Debug.Log("年齢：" + age);
+        Debug.Log("身長：" + height2);
+
+
+        for (i = 1; i < 20; i++)
+        {
+            hp -= damage;
+
+            Debug.Log("HP"+hp);
+           
+
+            if (hp == 0)
+            {
+                Debug.Log("あなたは"+i+"回目で死んでしまいました。");
+                break;
+            }
+        }
+
+
+
+    }
+
+
+// Update is called once per frame
+    void Update()
+    {
+
+    }
+}
