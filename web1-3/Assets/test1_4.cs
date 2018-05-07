37f15c65b3f2b8a27dbcdfe592d5d8cfa94e5f14
Unknown <g1827402@st.t-kougei.ac.jp>
2018-05-07 17:31:47 +0900

    web1-4、配列とクラス

---
+++ web1-3/Assets/test1_4.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player
+{
+    private int PlayerHP = 1000;
+    private int PlayerPower = 50;
+
+    public void Attack()
+    {
+        Debug.Log(this.PlayerPower + "のダメージ!");
+    }
+}
+
+
+
+
+public class test1_4 : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+
+        int[] points = new int[5];
+
+        Player player1 = new Player();
+
+        Vector2 pos1 = new Vector2(5.0f, 7.0f);
+        Vector2 pos2 = new Vector2(10.0f, 10.0f);
+        Vector2 posM = pos2 - pos1;
+
+        for(int i = 0; i < points.Length; i++)
+        {
+            points[i] = i * 10;
+
+            Debug.Log("points["+i+"] = "+ points[i]);
+        }
+
+        player1.Attack();
+
+        Debug.Log(posM);
+
+        float M = posM.magnitude;
+        Debug.Log(M);
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
